9e9ca11473e6ae91f91397d2cdff1d6a4f7c316d
Abram <israelvictory87@gmail.com>
2022-08-31 21:18:58 +0100

    Update setup configuration

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(
+    name='cloudinary-upload',
+    version='0.0.1',
+    description='Upload a signed image to cloudinary effectively',
+    long_description_content_type="text/markdown",
+    long_description=open('README.md').read(),
+    url='https://github.com/faradayafrica/cloudinary-image-upload', 
+    license='CC0 1.0',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: CC0 1.0 Universal (CC0 1.0) Public Domain Dedication',
+        'Programming Language :: Python :: 3'
+    ],
+    keywords=['cloudinary', 'media asset'],
+    install_requires=[
+        'requests',
+        'importlib-metadata; python_version == "3.8"',
+    ],
+)
