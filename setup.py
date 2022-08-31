a7d7581ac9eeed47de78b41048fe0622c7b9cb72
Abram <israelvictory87@gmail.com>
2022-08-31 21:57:48 +0100

    Bump version to 0.0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='cloudinary-upload',
-    version='0.0.1',
+    version='0.0.2',
     description='Upload a signed image to cloudinary effectively',
     long_description_content_type="text/markdown",
     long_description=open('README.md').read(),
